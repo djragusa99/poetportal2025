202970d08bcc17f50fff31075c26eda9a7c8578d
Damon Ragusa <109119041+djragusa99@users.noreply.github.com>
2025-01-05 21:56:33 +0000

    Assistant checkpoint: Fix unfollow notification to show correct message
    Assistant generated file changes:
    - client/src/components/PostCard.tsx: Fix unfollow notification message
    
    ---
    
    User prompt:
    
    Check again as I'm still getting the "followed user" when I'm expecting the "unfollowed user"

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -188,8 +188,7 @@ export default function PostCard({ post }: PostCardProps) {
     mutationFn: async () => {
       if (!user) throw new Error("Must be logged in to unfollow users");
       if (!post.user?.id) throw new Error("Invalid user ID");
-      await api.users.unfollow(post.user.id);
-      return { message: "Successfully unfollowed user" };
+      return await api.users.unfollow(post.user.id);
     },
     onSuccess: (data) => {
       queryClient.invalidateQueries({ queryKey: [`/api/users/${post.user?.id}/following`] });
